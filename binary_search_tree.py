05899528e954381f5f0f7ef0a470a0e453893ec3
Zaeem Akhtar <90152617+zmaktr@users.noreply.github.com>
2022-12-28 17:10:32 +0000

    min() max() and inorder_traverse() for binary search tree

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,3 +1,5 @@
+#"""
+#Option-1 
 class Node:
   def __init__(self, data):
     self.data = data
@@ -15,29 +17,118 @@ class BinarySearchTree:
     if self.root is None:
       self.root = Node(data)
     else:
-      parent = self.root
       currentNode = Node(data)
+      currentNode.parent = self.root
       while True:
-        if parent.data < data:
-          if parent.left_node is None:
-            parent.left_node = currentNode
+        if currentNode.parent.data > data:
+          if currentNode.parent.left_node is None:
+            currentNode.parent.left_node = currentNode
             break
-          parent = parent.left_node
-        elif parent.data > data:
-          if parent.right_node is None:
-            parent.right_node = currentNode
+          currentNode.parent = currentNode.parent.left_node
+        elif currentNode.parent.data < data:
+          if currentNode.parent.right_node is None:
+            currentNode.parent.right_node = currentNode
             break
-          parent = parent.right_node
+          currentNode.parent = currentNode.parent.right_node
         # duplicates in BST are not allowed so no equal (=) condition is placed
-              
+  
+  def max_node(self):
+    max_node = self.root
+    if max_node is not None:
+      while max_node.right_node is not None:
+        max_node = max_node.right_node
+    return print(max_node.data)
+
+  def min_node(self):
+    min_node = self.root
+    if min_node is not None:
+      while min_node.left_node is not None:
+        min_node = min_node.left_node
+    return print(min_node.data)
+
+  def inorder_traverse(self):
+    if self.root is not None:
+      self.inorder_recursion(self.root)
+
+  def inorder_recursion(self, node):
+    if node.left_node is not None:
+      self.inorder_recursion(node.left_node)
+
+    print(node.data)
+
+    if node.right_node is not None:
+      self.inorder_recursion(node.right_node)
+                
+#"""
+
+"""
+#Option-2
+class Node:
+  def __init__(self, data, parent=None):
+    self.data = data
+    self.right_node = None
+    self.left_node = None
+    self.parent = parent
+
+class BinarySearchTree:
+  def __init__(self):
+    self.root = None
+
+  def insert(self, data):
+    if self.root is None:
+      self.root = Node(data)
+    else:
+      self.insert_node(data, self.root)
+
+  def insert_node(self, data, node):
+    if data < node.data:
+      if node.left_node is not None:
+        self.insert_node(data, node.left_node)
+      else:
+        node.left_node = Node(data, node)
+    else:
+      if node.right_node is not None:
+        self.insert_node(data, node.right_node)
+      else:
+        node.right_node = Node(data, node)
+
+  def inorder_traverse(self):
+    if self.root is not None:
+      self.traverse(self.root)
+
+  def inorder_recursion(self, node):
+    if node.left_node is not None:
+      self.traverse(node.left_node)
+    
+    print(node.data)
+    
+    if node.right_node is not None:
+      self.traverse(node.right_node)
+
+"""
           
 
 i = BinarySearchTree()
-i.insert(2) 
-i.insert(1) 
-i.insert(4) 
-i.insert(6) 
-i.insert(5) 
-i.insert(23) 
-i.insert(67) 
+i.insert(50)
+i.insert(2)
+i.insert(5)
+i.insert(10)
+i.insert(15) 
+i.insert(20) 
+i.insert(25) 
+i.insert(30) 
+i.insert(35) 
+i.insert(40) 
+i.insert(45) 
+i.insert(55) 
+i.insert(60) 
+i.insert(65) 
+i.insert(70) 
+i.insert(75) 
+i.insert(80) 
+i.insert(85) 
 i.insert(90) 
+i.insert(95) 
+#i.min_node()
+#i.max_node()
+i.inorder_traverse()
